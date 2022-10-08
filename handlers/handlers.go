947a1d7d3b0ee0dfcac95143bf21e6b44ccbf170
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-08 15:00:18 +0000

    fix: check http method

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -7,27 +7,52 @@ import (
 )
 
 func HelloHandler(writer http.ResponseWriter, req *http.Request) {
-	io.WriteString(writer, "Hello World!\n")
+	if req.Method == http.MethodGet {
+		io.WriteString(writer, "Hello World!\n")
+	} else {
+		http.Error(writer, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
 
 func PostArticleHandler(writer http.ResponseWriter, req *http.Request) {
-	io.WriteString(writer, "Posting Article...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(writer, "Posting Article...\n")
+	} else {
+		http.Error(writer, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
 
 func ListArticlesHandler(writer http.ResponseWriter, req *http.Request) {
-	io.WriteString(writer, "Article List\n")
+	if req.Method == http.MethodGet {
+		io.WriteString(writer, "Article List\n")
+	} else {
+		http.Error(writer, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
 
 func ArticleDetailHandler(writer http.ResponseWriter, req *http.Request) {
 	articleId := 1
 	body := fmt.Sprintf("Article No.%d\n", articleId)
-	io.WriteString(writer, body)
+
+	if req.Method == http.MethodGet {
+		io.WriteString(writer, body)
+	} else {
+		http.Error(writer, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
 
 func PostNiceHandler(writer http.ResponseWriter, req *http.Request) {
-	io.WriteString(writer, "Posting Nice...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(writer, "Posting Nice...\n")
+	} else {
+		http.Error(writer, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
 
 func PostCommentHandler(writer http.ResponseWriter, req *http.Request) {
-	io.WriteString(writer, "Posting Comment...\n")
+	if req.Method == http.MethodPost {
+		io.WriteString(writer, "Posting Comment...\n")
+	} else {
+		http.Error(writer, "Invalid Method", http.StatusMethodNotAllowed)
+	}
 }
