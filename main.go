627f87d910a662f88f65cd8b6769e941e70552bf
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-08 15:06:59 +0000

    refactor: use mux method limit

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"try-go/handlers"
 )
 
 func main() {
-	http.HandleFunc("/hello", handlers.HelloHandler)
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/article/list", handlers.ListArticlesHandler)
-	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
-	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
-	http.HandleFunc("/comment", handlers.PostCommentHandler)
+	router := mux.NewRouter()
+
+	router.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	router.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
+	router.HandleFunc("/article/list", handlers.ListArticlesHandler).Methods(http.MethodGet)
+	router.HandleFunc("/article/1", handlers.ArticleDetailHandler).Methods(http.MethodGet)
+	router.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+	router.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
 
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
