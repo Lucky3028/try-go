76f8822228f018d44c5d7cadac23de72872430e9
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-08 15:19:55 +0000

    feat: receive page query in `/list`

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -18,7 +18,22 @@ func PostArticleHandler(writer http.ResponseWriter, req *http.Request) {
 }
 
 func ListArticlesHandler(writer http.ResponseWriter, req *http.Request) {
-	io.WriteString(writer, "Article List\n")
+	queryMap := req.URL.Query()
+
+	var page int
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+		if err != nil {
+			http.Error(writer, "Invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+
+	body := fmt.Sprintf("Article List (page %d)\n", page)
+	io.WriteString(writer, body)
 }
 
 func ArticleDetailHandler(writer http.ResponseWriter, req *http.Request) {
