085bb0e12c367e301d2523974564367cc0b0f9cb
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-09 14:59:45 +0000

    chore: use testdata/*.sql

--- repositories/testdata/setup.sql
+++ repositories/testdata/setup.sql
@@ -15,3 +15,13 @@ create table if not exists comments (
 
   foreign key (article_id) references articles(article_id)
 );
+
+insert into articles (title, contents, username, nice, created_at) values
+  ("firstPost", "This is my first blog.", "Me", 0, now());
+insert into articles (title, contents, username, nice, created_at) values
+  ("secondPost", "This is my second blog.", "Me", 0, now());
+
+insert into comments (article_id, message, created_at) values
+  (1, "1 get", now());
+insert into comments (article_id, message, created_at) values
+  (1, "Welcome to the Internet.", now());
--- sql/v1.1.0__Insert_test_data.sql
+++
@@ -1,9 +0,0 @@
-insert into articles (title, contents, username, nice, created_at) values
-  ("firstPost", "This is my first blog.", "Me", 0, now());
-insert into articles (title, contents, username, nice, created_at) values
-  ("secondPost", "This is my second blog.", "Me", 0, now());
-
-insert into comments (article_id, message, created_at) values
-  (1, "1 get", now());
-insert into comments (article_id, message, created_at) values
-  (1, "Welcome to the Internet.", now());
