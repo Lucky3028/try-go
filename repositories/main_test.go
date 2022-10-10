78c194eee129f923d1fbc15632bdf04bf4a57b10
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-10 03:41:07 +0000

    refactor(test): cleanup and setup db in each test

--- repositories/main_test.go
+++ repositories/main_test.go
@@ -4,19 +4,22 @@ import (
 	"database/sql"
 	"fmt"
 	"os"
+	"os/exec"
 	"testing"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var testDb *sql.DB
+var (
+	dbUser        = "docker"
+	dbPassword    = "docker"
+	dbDatabase    = "sampledb"
+	containerName = "mysql"
+	dbConn        = fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?parseTime=true", dbUser, dbPassword, containerName, dbDatabase)
+	testDb        *sql.DB
+)
 
-func setup() error {
-	dbUser := "docker"
-	dbPassword := "docker"
-	dbDatabase := "sampledb"
-	dbConn := fmt.Sprintf("%s:%s@tcp(mysql:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
-
+func connectToDb() error {
 	var err error
 	testDb, err = sql.Open("mysql", dbConn)
 	if err != nil {
@@ -26,7 +29,42 @@ func setup() error {
 	return nil
 }
 
+func setupTestData() error {
+	cmd := exec.Command("docker", "exec", containerName, "mysql", "-u", "docker", "sampledb", "--password=docker", "-e", "source /sql/setup.sql")
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func cleanupDb() error {
+	cmd := exec.Command("docker", "exec", containerName, "mysql", "-u", "docker", "sampledb", "--password=docker", "-e", "source /sql/cleanup.sql")
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func setup() error {
+	if err := connectToDb(); err != nil {
+		return err
+	}
+	if err := cleanupDb(); err != nil {
+		fmt.Println("cleanup", err)
+		return err
+	}
+	if err := setupTestData(); err != nil {
+		fmt.Println("setup", err)
+		return err
+	}
+
+	return nil
+}
+
 func teardown() {
+	cleanupDb()
 	testDb.Close()
 }
 
