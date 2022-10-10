06d81bb595e1cf4b1db4a913490d497575a4e042
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-10 05:40:06 +0000

    refactor: rename handlers -> controllers

--- main.go
+++ main.go
@@ -4,19 +4,19 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/Lucky3028/try-go/handlers"
+	"github.com/Lucky3028/try-go/controllers"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-	router.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-	router.HandleFunc("/article/list", handlers.ListArticlesHandler).Methods(http.MethodGet)
-	router.HandleFunc("/article/{id:[1-9][0-9]*}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-	router.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
-	router.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+	router.HandleFunc("/hello", controllers.HelloHandler).Methods(http.MethodGet)
+	router.HandleFunc("/article", controllers.PostArticleHandler).Methods(http.MethodPost)
+	router.HandleFunc("/article/list", controllers.ListArticlesHandler).Methods(http.MethodGet)
+	router.HandleFunc("/article/{id:[1-9][0-9]*}", controllers.ArticleDetailHandler).Methods(http.MethodGet)
+	router.HandleFunc("/article/nice", controllers.PostNiceHandler).Methods(http.MethodPost)
+	router.HandleFunc("/comment", controllers.PostCommentHandler).Methods(http.MethodPost)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
