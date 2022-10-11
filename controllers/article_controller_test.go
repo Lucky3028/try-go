829aa034933dd1a74956709ba4eee1ed5df36980
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-10-11 06:50:59 +0000

    test: add TestArticleDetailHandler

--- controllers/article_controller_test.go
+++ controllers/article_controller_test.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
 func TestListArticlesHandler(t *testing.T) {
@@ -23,6 +25,33 @@ func TestListArticlesHandler(t *testing.T) {
 			request := httptest.NewRequest(http.MethodGet, url, nil)
 			response := httptest.NewRecorder()
 			articleController.ListArticlesHandler(response, request)
+
+			if response.Code != tt.resultCode {
+				t.Errorf("unexpected StatusCode: want %d but %d\n", tt.resultCode, response.Code)
+			}
+		})
+	}
+}
+
+func TestArticleDetailHandler(t *testing.T) {
+	var tests = []struct {
+		name       string
+		articleID  string
+		resultCode int
+	}{
+		{name: "number pathparam", articleID: "1", resultCode: http.StatusOK},
+		{name: "alphabet pathparam", articleID: "aaa", resultCode: http.StatusNotFound},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			url := fmt.Sprintf("http://localhost:8080/article/%s", tt.articleID)
+			request := httptest.NewRequest(http.MethodGet, url, nil)
+			response := httptest.NewRecorder()
+
+			router := mux.NewRouter()
+			router.HandleFunc("/article/{id:[1-9][0-9]*}", articleController.ArticleDetailHandler).Methods(http.MethodGet)
+			router.ServeHTTP(response, request)
 
 			if response.Code != tt.resultCode {
 				t.Errorf("unexpected StatusCode: want %d but %d\n", tt.resultCode, response.Code)
